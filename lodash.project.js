46b1ea05ab41e58efb2263f7c0ea429fcd6c3281
Giancarlo Palavicini <kasthor@gmail.com>
2019-04-23 13:01:12 -0600

    fix: get recursion working

--- lodash.project.js
+++ lodash.project.js
@@ -1,23 +1,23 @@
 const _ = require('lodash');
 
-module.exports = {
+const mx = {
   project(obj, projection) {
     const replacementRegex = /\${(.*)}/;
 
     return _.mapValues(projection, (value) => {
       if (_.isObject(value)) {
         if ('$array' in value) {
-          return _.map(_.get(obj, _.get(value, '$array')), i => _.project(i, _.get(value, '$template')));
+          return _.map(_.get(obj, _.get(value, '$array')), i => mx.project(i, _.get(value, '$template')));
         } else if ('$object_from_array' in value) {
           return _(_.get(obj, _.get(value, '$object_from_array')))
             .map(i => [
               _.get(i, _.get(value, '$key')),
-              _.project(i, _.get(value, '$template')),
+              mx.project(i, _.get(value, '$template')),
             ])
             .fromPairs()
             .value();
         } else {
-          return _.project(obj, value);
+          return mx.project(obj, value);
         }
       } else if (_.isString(value)) {
         const m = value.match(replacementRegex);
@@ -28,3 +28,5 @@ module.exports = {
     });
   },
 };
+
+module.exports = mx;
